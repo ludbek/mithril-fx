a39b5b78359a86b06b9496ee3b5400afab33295a
ludbek <sth.srn@gmail.com>
2016-04-26 12:31:55 +0545

    css animation done

--- index.js
+++ index.js
@@ -8,141 +8,134 @@ var getVDOM = function (args) {
   return m.apply(m, args)
 }
 
+var getArray = function (arrayLike) {
+  var args = []
+
+  for (var i = 1, length = arrayLike.length; i < length; i++) {
+    args[i - 1] = arrayLike[i]
+  }
+  return args
+}
+
+var events = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
+
 module.exports = function () {
-  function mountUnmount() {
+  // fx(node, attrs, data)
+  if (_.isString(arguments[0]) && arguments.length >= 2) {
     var vdom = getVDOM(arguments)
 
-    var attachAnimation = function (el, initialized, ctx)  {
+    var animate = function (el, initialized, ctx) {
       var dom = $(el)
-
       if (initialized) return
-
-      // before initialization
-      dom.addClass('fx add')
-        .one('animationend', function () {dom.removeClass('fx add')})
-
-      ctx.onunload = function () {
-        m.startComputation()
-        dom.addClass('fx remove')
-          .one('animationend', function () {
-            dom.removeClass('fx remove')
-            m.endComputation()
-          })
-      }
+      dom.addClass('fx mount')
+        .one(events, function () {dom.removeClass('fx mount')})
     }
 
     // attach animate to attrs.config
     var originalConfig = vdom.attrs.config
-    vdom.attrs.config = function (el, initialized, ctx, dom) {
-      if (originalConfig) originalConfig(el, initialized, ctx, dom)
-      attachAnimation(el, initialized, ctx)
+    vdom.attrs.config = function (el, init, ctx, dom) {
+      if (originalConfig) originalConfig.call(vdom, el, init, ctx, dom)
+      animate.call(vdom, el, init, ctx, dom)
     }
 
     return m(vdom.tag, vdom.attrs, vdom.children)
   }
+  // fx(boolean, node, attrs, data)
+  else if (_.isBoolean(arguments[0])) {
+    var value = arguments[0]
+    var vdom = getVDOM(getArray(arguments))
 
-  // m(node, attrs, data)
-  if (_.isString(arguments[0]) && arguments.length >= 2) {
-    return mountUnmount.apply(mountUnmount, arguments)
+    var animate = function (el, initialized, ctx)  {
+      var dom = $(el)
+
+      if(!initialized) {
+        if (value) {
+          dom.css('display', 'block')
+          }
+        else {
+          dom.css('display', 'none')
+          }
+        ctx.state = value
+        }
+
+      if (ctx.state === value) return
+      if (value) {
+        var className = 'fx show'
+        dom.css('display', 'block')
+        dom.addClass(className)
+          .one(events, function () {dom.removeClass(className)})
+        }
+      else {
+        var className = 'fx hide'
+        dom.addClass(className)
+          .one(events, function () {
+            dom.removeClass(className)
+            dom.css('display', 'none')
+          })
+        }
+
+      ctx.state = value
+    }
+
+    var originalConfig = vdom.attrs.config
+    vdom.attrs.config = function (el, init, ctx, dom) {
+      if (originalConfig) originalConfig.call(vdom, el, init, ctx, dom)
+      animate.call(vdom, el, init, ctx, dom)
+    }
+
+    return m(vdom.tag, vdom.attrs, vdom.children)
   }
+  // fx(array, callback)
+  else if(_.isArray(arguments[0], arguments.length === 2)) {
+    var data = arguments[0], callback = arguments[1]
 
+    var elements = _.map(data, callback)
 
-  // //hide show
-  // function toggle() {
-  //   var vdom = getVDOM(arguments)
-  //   if (vdom.attrs._toggle !== false || vdom.attrs.boolean !== true) new Error('toggle requires a "attrs.boolean" field.');
+    var animate = function (index, data, config) {
+      return function (el, initialized, ctx) {
+        var dom = $(el)
+        var addClass = 'fx mount'
+        var changeClass = 'fx change'
+        var moveHigh = 'fx move high'
+        var moveLow = 'fx move low'
 
-  //   var value = vdom.attrs._toggle;
+        // execute original config
+        if (config) config(el, initialized, ctx)
 
-  //   var animate = function (el, initialized, ctx)  {
-  //     var dom = $(el);
+        // for addition of element
+        if(!initialized) {
+          dom.addClass(addClass)
+            .one(events, function () {dom.removeClass(addClass)})
+        }
+        // for element move
+        if ((ctx.index || ctx.index === 0) && ctx.index < index) {
+          dom.addClass(moveLow)
+            .one(events, function () {dom.removeClass(moveLow)})
+        }
+        else if ((ctx.index || ctx.index === 0) && ctx.index > index) {
+          dom.addClass(moveHigh)
+            .one(events, function () {dom.removeClass(movieHigh)})
+        }
+        // if change in data
+        if (ctx.data && !_.isEqual(ctx.data, data)) {
+          dom.addClass(changeClass)
+            .one(events, function () {dom.removeClass(changeClass)})
+        }
+        // save state
+        ctx.data = _.clone(data, true)
+        ctx.index = index
+      }
+    }
 
-  //     if(!initialized) {
-  //       if (value) {
-  //         dom.css('display', 'block');
-  //         }
-  //       else {
-  //         dom.css('display', 'none');
-  //         }
-  //       ctx.state = value;
-  //       }
+    // attach animation to elements
+    return _.map(_.range(data.length), function (index) {
+      var delement = elements[index], ddata = data[index]
+      var originalConfig = delement.attrs.config
 
-  //     if (ctx.state === value) return;
-  //     if (value) {
-  //       var className = 'animation show';
-  //       dom.css('display', 'block');
-  //       dom.addClass(className)
-  //         .one('animationend', function () {dom.removeClass(className);});
-  //       }
-  //     else {
-  //       var className = 'animation hide';
-  //       dom.addClass(className)
-  //         .one('animationend', function () {dom.removeClass(className);dom.css('display', 'none');});
-  //       }
+      delement.attrs.key = ddata.id || ddata
+      delement.attrs.config = animate(index, ddata, originalConfig)
 
-  //     ctx.state = value;
-  //     };
-
-  //   // attach animate to attrs.config
-  //   var originalConfig = vdom.attrs.config;
-  //   vdom.attrs.config = function (el, initialized, ctx) {
-  //     if (originalConfig) originalConfig(el, initialized, ctx);
-  //     animate(el, initialized, ctx);
-  //   };
-
-  //   return m(vdom.tag, vdom.attrs, vdom.children);
-  //   }
-
-
-  // // mount, unmount, change, move low, move high
-  // function collection(data, callback) {
-  //   if (!(data && callback)) new Error('map needs at least two arguments.');
-
-  //   var elements = _.map(data, callback);
-  //   // armc animator
-  //   var armc = function (index, data, config) {
-  //     return function (el, initialized, ctx) {
-  //       var dom = $(el);
-  //       var addClass = 'animation add';
-  //       var changeClass = 'animation change';
-
-  //       // execute original config
-  //       if (config) config(el, initialized, ctx);
-
-  //       // for addition of element
-  //       if(!initialized) {
-  //         dom.addClass(addClass)
-  //           .one('animationend', function () {dom.removeClass(addClass)});
-  //       }
-  //       // for element move
-  //       if ((ctx.index || ctx.index === 0) && ctx.index < index) {
-  //         dom.addClass('animation move low')
-  //           .one('animationend', function () {dom.removeClass('animation move low')});
-  //       }
-  //       else if ((ctx.index || ctx.index === 0) && ctx.index > index) {
-  //         dom.addClass('animation move high')
-  //           .one('animationend', function () {dom.removeClass('animation move high')});
-  //       }
-  //       // if change in data
-  //       if (ctx.data && !_.isEqual(ctx.data, data)) {
-  //         dom.addClass(changeClass)
-  //           .one('animationend', function () {dom.removeClass(changeClass)});
-  //       }
-  //       // save state
-  //       ctx.data = _.clone(data, true);
-  //       ctx.index = index;
-  //     };
-  //   };
-
-  //   // attach animation to elements
-  //   return _.map(_.range(data.length), function (index) {
-  //     var delement = elements[index], ddata = data[index];
-  //     var originalConfig = delement.attrs.config;
-
-  //     delement.attrs.key = ddata.id || ddata;
-  //     delement.attrs.config = armc(index, ddata, originalConfig);
-
-  //     return delement;
-  //   });
-  // }
+      return delement
+    })
+  }
 }
